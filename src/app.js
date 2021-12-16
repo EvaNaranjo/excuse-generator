9a22f59a6c2d101ad3827594450af735a36016d2
Eva Naranjo <evita.naranjo@gmail.com>
2021-12-16 10:06:33 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = [" The ghost", "My lion", "My grandgrandma", "My neighbour"];
+  let action = ["ate", "broke", "threw", "lost"];
+  let what = ["my computer", "my car", "my keys", "my trousers"];
+  let when = ["yesterday", "two minutes ago", "last night", "a second ago"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex] +
+    "!"
+  );
 };
